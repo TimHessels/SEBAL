f4b14b96b91c06d472ff22a951dae993b4c265ea
Tim Hessels <timhessels@hotmail.com>
2019-06-18 20:51:19 +0200

    small changes

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Authors: Tim Hessels
-         IHE Delft 2017
 Contact: t.hessels@un-ihe.org
-Repository: https://github.com/wateraccounting/SEBAL
+Repository: https://github.com/TimHessels/SEBAL
 Module: SEBAL
 
 
 Description:
 This module contains a compilation of scripts and functions to run pySEBAL
-(http://www.wateraccounting.org/)
 """
 
 
